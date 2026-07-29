7116a25d8a922e714084f3cd694d2a37c9a79f60
umut eray altay <umut619umut1963@gmail.com>
2026-07-29 15:32:28 +0300

    TWA projesi oluşturuldu (bubblewrap init) - com.umuterayaltay.sosyal, host: sosyalmedyadeneme.onrender.com

---
+++ app/src/main/java/com/umuterayaltay/sosyal/DelegationService.java
@@ -0,0 +1,14 @@
+package com.umuterayaltay.sosyal;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
